f7f6dbe7572878b28247421819a365370827b196
NeilRow56 <neilrowland56@gmail.com>
2025-07-28 14:34:45 +0100

    Home page complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import CallToAction from '@/components/call-to-action'
+import Features from '@/components/features'
+import FooterSection from '@/components/footer'
+
 import HeroSection from '@/components/hero-section'
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <Features />
+      <CallToAction />
+      <FooterSection />
     </main>
   )
 }
